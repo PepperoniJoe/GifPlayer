ef63b04644f011b35dc371b9c26236b48aeb03ea
Marcy <pepperonijo@gmail.com>
2020-07-21 00:17:48 -0500

    update project

--- GifPlayer/Data/AppData.swift
+++ GifPlayer/Data/AppData.swift
@@ -14,17 +14,17 @@ struct AppData {
   
   let gifData: [(name: String, stillImageNumber: Int, bgColor: UIColor, contentMode: UIView.ContentMode, loops: Int, speed: Double)] =
     [
-      ("Pig"             , 1,  #colorLiteral(red              : 0.5528588891, green              : 0.7616898417, blue              : 0.420261234, alpha              : 1),     .scaleAspectFit,  0, 1),
-      ("BlueMoon"        , 1,  .clear, .scaleToFill,     1, 1),
       ("Owl"             , 1,  #colorLiteral(red            : 0.424493134, green            : 0.3560110629, blue            : 0.5905451179, alpha            : 1),     .scaleAspectFit,  0, 1),
-      ("DogRun"          , 1,  #colorLiteral(red : 0.2392156869, green : 0.6745098233, blue : 0.9686274529, alpha : 1),     .scaleAspectFit,  0, 2.5),
-      ("Chick2"          , 1,  #colorLiteral(red           : 0.424493134, green           : 0.3560110629, blue           : 0.5905451179, alpha           : 1),     .scaleAspectFill, 0, 1),
+      ("BlueMoon"        , 1,  .clear, .scaleToFill,     1, 1),
+      ("Pig"             , 1,  #colorLiteral(red              : 0.5528588891, green              : 0.7616898417, blue              : 0.420261234, alpha              : 1),     .scaleAspectFit,  0, 1),
       ("DesertAnimation3", 1,  .clear, .scaleToFill,     1, 1),
-      ("Panda"           , 30, #colorLiteral(red         : 0.7893047929, green         : 0.4369269609, blue         : 0.5225962996, alpha         : 1),     .scaleAspectFit,  0, 1),
-      ("CloudySunColored", 1,  #colorLiteral(red : 0.2392156869, green : 0.6745098233, blue : 0.9686274529, alpha : 1),     .scaleAspectFit,  1, 1),
+      ("Chick2"          , 1,  #colorLiteral(red           : 0.424493134, green           : 0.3560110629, blue           : 0.5905451179, alpha           : 1),     .scaleAspectFill, 0, 1),
+      ("DogRun"          , 1,  #colorLiteral(red : 0.2392156869, green : 0.6745098233, blue : 0.9686274529, alpha : 1),     .scaleAspectFit,  0, 2.5),
       ("Yesterday"       , 1,  #colorLiteral(red         : 0.5528588891, green         : 0.7616898417, blue         : 0.420261234, alpha         : 1),     .scaleAspectFit,  1, 1),
-      ("LandscapeScroll" , 1,  #colorLiteral(red  : 0.424493134, green  : 0.3560110629, blue  : 0.5905451179, alpha  : 1),     .scaleToFill,     0, 1),
-      ("Cat"             , 1,  #colorLiteral(red              : 0.5528588891, green              : 0.7616898417, blue              : 0.420261234, alpha              : 1),     .scaleAspectFit,  0, 1),
+      ("CloudySunColored", 1,  #colorLiteral(red : 0.2392156869, green : 0.6745098233, blue : 0.9686274529, alpha : 1),     .scaleAspectFit,  1, 1),
+      ("Panda"           , 30, #colorLiteral(red         : 0.7893047929, green         : 0.4369269609, blue         : 0.5225962996, alpha         : 1),     .scaleAspectFit,  0, 1),
       ("GoldDog"         , 73, .black, .scaleAspectFit,  1, 1),
+      ("Cat"             , 1,  #colorLiteral(red              : 0.5528588891, green              : 0.7616898417, blue              : 0.420261234, alpha              : 1),     .scaleAspectFit,  0, 1),
+      ("LandscapeScroll" , 1,  #colorLiteral(red  : 0.424493134, green  : 0.3560110629, blue  : 0.5905451179, alpha  : 1),     .scaleToFill,     0, 1),
     ]
 }
--- README.md
+++ README.md
@@ -3,16 +3,23 @@
 [![Language](https://img.shields.io/badge/swift-5.0-orange.svg)](https://developer.apple.com/swift)
 [![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](http://mit-license.org)
 
-A native iOS app that plays GIF files as an animated image. 
+A Swift app that plays GIF files as an animated image. 
 
-**Inspiration**
+**Keynote**
+Any gif file can be used. All the gif files of this app were created with Apple's Keynote including all artwork\
+and animations. Keynote's shimmer, jiggles and other special effects were used. Keynotes magic moves and\
+transitions were used to create endless scrolling landscapes, scrolling song lyrics and animal animations. 
 
-
-<img src="GitHub-Images/HexKeyboard.gif" width="300">
+<img src="GitHub-Images/GifPlayer.gif" width="300">
 
 
 **Technical Info:** \
 Xcode12  project written in Swift 5 for iOS\
+GIF files, animation, tap gestures, collection views, tab bar controller\
+collection view flow layout, imageview.animationImages, storyboard references\
+data assets, CGImageSourceGetCount, CGImageSourceCreateImageAtIndex\
+UIImage.animatedImage, color assets\
+Apple Keynote animations including magic move, rotate, shimmer, bounce, jiggle
 
 
 **See Also:** \
@@ -23,5 +30,7 @@ Xcode12  project written in Swift 5 for iOS\
 **Author:** \
 Marcy Vernon [@MarcyVernon](https://twitter.com/MarcyVernon)
 
+
 **License:** \
-"Hexadecimal Keyboard" is under the MIT license. See [LICENSE](/LICENSE) for more information.
+"GifPlayer" software is under the MIT license. See [LICENSE](/LICENSE) for more information.
+GIF files and art content Copyright (c) 2020 Marcy Vernon
