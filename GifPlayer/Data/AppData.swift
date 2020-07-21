10050aabc2fb21efcd4edc84e0b8d663dde55d0c
Marcy <pepperonijo@gmail.com>
2020-07-20 23:17:41 -0500

    Update project

---
+++ GifPlayer/Data/AppData.swift
@@ -0,0 +1,30 @@
+//
+//  AppData.swift
+//  MovPlayer
+//
+//  Created by Marcy Vernon on 11/4/19.
+//  Copyright © 2019 Marcy Vernon. All rights reserved.
+//
+
+import UIKit
+
+// Gifs to display, in the order listed.
+
+struct AppData {
+  
+  let gifData: [(name: String, stillImageNumber: Int, bgColor: UIColor, contentMode: UIView.ContentMode, loops: Int, speed: Double)] =
+    [
+      ("Pig"             , 1,  #colorLiteral(red              : 0.5528588891, green              : 0.7616898417, blue              : 0.420261234, alpha              : 1),     .scaleAspectFit,  0, 1),
+      ("BlueMoon"        , 1,  .clear, .scaleToFill,     1, 1),
+      ("Owl"             , 1,  #colorLiteral(red            : 0.424493134, green            : 0.3560110629, blue            : 0.5905451179, alpha            : 1),     .scaleAspectFit,  0, 1),
+      ("DogRun"          , 1,  #colorLiteral(red : 0.2392156869, green : 0.6745098233, blue : 0.9686274529, alpha : 1),     .scaleAspectFit,  0, 2.5),
+      ("Chick2"          , 1,  #colorLiteral(red           : 0.424493134, green           : 0.3560110629, blue           : 0.5905451179, alpha           : 1),     .scaleAspectFill, 0, 1),
+      ("DesertAnimation3", 1,  .clear, .scaleToFill,     1, 1),
+      ("Panda"           , 30, #colorLiteral(red         : 0.7893047929, green         : 0.4369269609, blue         : 0.5225962996, alpha         : 1),     .scaleAspectFit,  0, 1),
+      ("CloudySunColored", 1,  #colorLiteral(red : 0.2392156869, green : 0.6745098233, blue : 0.9686274529, alpha : 1),     .scaleAspectFit,  1, 1),
+      ("Yesterday"       , 1,  #colorLiteral(red         : 0.5528588891, green         : 0.7616898417, blue         : 0.420261234, alpha         : 1),     .scaleAspectFit,  1, 1),
+      ("LandscapeScroll" , 1,  #colorLiteral(red  : 0.424493134, green  : 0.3560110629, blue  : 0.5905451179, alpha  : 1),     .scaleToFill,     0, 1),
+      ("Cat"             , 1,  #colorLiteral(red              : 0.5528588891, green              : 0.7616898417, blue              : 0.420261234, alpha              : 1),     .scaleAspectFit,  0, 1),
+      ("GoldDog"         , 73, .black, .scaleAspectFit,  1, 1),
+    ]
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# GIFPlayer
+[![Xcode version](https://img.shields.io/badge/xcode-12%20-brightgreen)](https://developer.apple.com/xcode/)
+[![Language](https://img.shields.io/badge/swift-5.0-orange.svg)](https://developer.apple.com/swift)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](http://mit-license.org)
+
+A native iOS app that plays GIF files as an animated image. 
+
+**Inspiration**
+
+
+<img src="GitHub-Images/HexKeyboard.gif" width="300">
+
+
+**Technical Info:** \
+Xcode12  project written in Swift 5 for iOS\
+
+
+**See Also:** \
+[Quickies - a variety of example iOS apps](https://github.com/PepperoniJoe/Quickies)\
+ [Make Icons Swiftly - used to generate app icon sets
+ ](https://github.com/PepperoniJoe/Make-Icons-Swiftly)
+ 
+**Author:** \
+Marcy Vernon [@MarcyVernon](https://twitter.com/MarcyVernon)
+
+**License:** \
+"Hexadecimal Keyboard" is under the MIT license. See [LICENSE](/LICENSE) for more information.
